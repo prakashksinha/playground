31183ce42765a816d849b4445ef6f3bd6d42de8d
Prakash <prakashksinha@gmail.com>
2013-12-08 16:26:36 -0500

    Delete streetofwalls_ddl.sql

--- streetofwalls/sql/streetofwalls_ddl.sql
+++
@@ -1,5 +0,0 @@
-CREATE DATABASE IF NOT EXISTS streetofwalls
-  DEFAULT CHARACTER SET utf8
-  DEFAULT COLLATE utf8_general_ci;
-
-GRANT ALL PRIVILEGES ON streetofwalls.* TO streetofwalls@localhost IDENTIFIED BY 'str33t0fw@!!2';
